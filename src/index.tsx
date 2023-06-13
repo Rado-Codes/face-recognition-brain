009cda86340ce323239ce24c2e3cd2a7c8cc40b5
Radoslaw <125380891+Rado-Codes@users.noreply.github.com>
2023-06-13 13:51:55 +0200

    Typescript transition from CRA

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+  );
+  root.render(
 	<React.StrictMode>
-		<App />
+	  <App />
 	</React.StrictMode>
-);
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
